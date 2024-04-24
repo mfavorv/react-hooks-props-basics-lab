77972ba099bafcf9958b9e731c22291486e1c533
Favor Vincent <vfavorm77@gmail.com>
2024-04-24 13:16:22 +0300

    REACT props basics lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Links from "./Links.js"
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+
       {/* add your <Links /> component here */}
+      {props.bio && props.bio.trim() !== "" ? <p>{props.bio}</p> : null}
+      <Links github={props.github} linkedin={props.linkedin} />
     </div>
   );
 }
